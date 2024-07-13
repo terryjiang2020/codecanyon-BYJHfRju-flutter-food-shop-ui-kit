607f9b20f3eed516298eba0d109a5dd1adcb6b5b
TerryAtRocketVentures <68800619+terryjiang2020@users.noreply.github.com>
2024-07-13 17:23:44 +1200

    save progress

--- food_market/lib/main.dart
+++ food_market/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -9,20 +11,37 @@ import 'package:food_market/providers/theme_provider.dart';
 import 'package:food_market/route_management.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<dynamic> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   await prefs.setBool('darkTheme', false);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, this.home});
+  MyApp({super.key, this.home});
 
   final String? home;
+  final screenshotController = ScreenshotController();
 
+  void pressHandler() {
+    print('Button pressed, updated 3');
+    screenshotController
+    .capture(delay: const Duration(milliseconds: 100))
+    .then((image) {
+      if (image != null) {
+        print('Capture Done');
+      } else {
+        print('Capture Failed');
+      }
+    }).catchError((onError) {
+      print('Capture Error: $onError');
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -51,6 +70,22 @@ class MyApp extends StatelessWidget {
             supportedLocales: L10n.all,
             initialRoute: home ?? Routes.splash,
             getPages: allPages,
+            builder: (context, child) {
+              return Scaffold(
+                body: Screenshot(
+                  controller: screenshotController,
+                  child: child,
+                ),
+                floatingActionButton: FloatingActionButton(
+                  // onPressed: () {
+                  //   pressHandler();
+                  // },
+                  onPressed: ()=>Timer(Duration(milliseconds: 10),pressHandler),
+                  child: const Icon(Icons.add),
+                ),
+                floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+              );
+            },
           );
         },
       ),
